04984c50b0f0b77d351fe4a745aea43c3f7b832d
Anton-Sleptsov <arslepcov@kait20.ru>
2024-02-26 23:23:59 +0300

    Первый урок

---
+++ js/modal.js
@@ -0,0 +1,16 @@
+const modal = document.querySelector(".search-model");
+const buttonOpen = document.querySelector(".icon_search");
+const buttonClose = modal.querySelector(".search-close-switch");
+const searchString = modal.querySelector("#search-input");
+
+buttonOpen.addEventListener('click', () => {
+  modal.style.display = "block";
+});
+
+buttonClose.addEventListener('click', () => {
+  modal.style.display = "none";
+});
+
+searchString.addEventListener('input', () => {
+  console.log(searchString.value);
+});
