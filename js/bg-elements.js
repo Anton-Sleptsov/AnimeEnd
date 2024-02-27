12f23bec372b4234634573763733f7c0be5bcd0c
Anton-Sleptsov <arslepcov@kait20.ru>
2024-02-27 13:29:37 +0300

    Второй урок

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -1,8 +1,12 @@
-const elements = document.querySelectorAll(".set-bg");
+const bgElements = () => {
+  const elements = document.querySelectorAll(".set-bg");
 
-for (let index = 0; index < elements.length; index++) {
-  const element = elements[index];
+  for (let index = 0; index < elements.length; index++) {
+    const element = elements[index];
 
-  const src = element.dataset.setbg;
-  element.style.backgroundImage = `url(${src})`;
+    const src = element.dataset.setbg;
+    element.style.backgroundImage = `url(${src})`;
+  }
 }
+
+bgElements();
--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,20 @@
-const modal = document.querySelector(".search-model");
-const buttonOpen = document.querySelector(".icon_search");
-const buttonClose = modal.querySelector(".search-close-switch");
-const searchString = modal.querySelector("#search-input");
+const modal = () => {
+  const modal = document.querySelector(".search-model");
+  const buttonOpen = document.querySelector(".icon_search");
+  const buttonClose = modal.querySelector(".search-close-switch");
+  const searchString = modal.querySelector("#search-input");
 
-buttonOpen.addEventListener('click', () => {
-  modal.style.display = "block";
-});
+  buttonOpen.addEventListener('click', () => {
+    modal.style.display = "block";
+  });
 
-buttonClose.addEventListener('click', () => {
-  modal.style.display = "none";
-});
+  buttonClose.addEventListener('click', () => {
+    modal.style.display = "none";
+  });
 
-searchString.addEventListener('input', () => {
-  console.log(searchString.value);
-});
+  searchString.addEventListener('input', () => {
+    console.log(searchString.value);
+  });
+}
+
+modal();
